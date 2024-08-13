e6e12b8076c4eb4b0b84252391231c78a4d4b423
felixmathew03 <felixmathewtt@gmail.com>
2024-08-13 09:11:05 +0530

    6

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,5 @@
+
+let qu
 function getCart(){
     // console.log(localStorage.length);
     str=``;
@@ -7,14 +9,24 @@ function getCart(){
         const value=JSON.parse(localStorage.getItem(key));
         // console.log(value);
         str+=`<div class="cart">
-            
-                <img src="${value.thumbnail}" alt="">
-            
-            <h2>${value.title}</h2>
-            
-                <button onclick="removeCart('${value.id}')">Remove</button>
-            
-        </div>`
+                <div class="imge">
+                    <img src="../img/c22c9fc4-0555-4460-8401-bf5c28d7ba29.webp" alt="">
+                </div>
+                <div class="content">
+                    <h4>${value.title}</h4>
+                    <h3>${value.price}</h3>
+                    <p>${value.description}</p>
+                    <p>Quantity</p>
+                    <p>
+                        <span class="qnty" onclick="minus()"><img src="../img/remove_16dp_FFFFFF_FILL0_wght400_GRAD0_opsz20.png" alt=""></span>
+                        <span class="txt">1</span>
+                        <span class="qnty" onclick="javascript:alert('event has been triggered');"><img src="../img/add_16dp_FFFFFF_FILL0_wght400_GRAD0_opsz20.png" alt=""></span>
+                    </p>
+                </div>           
+                <div class="bun">
+                    <button onclick="removeCart('${value.id}')">Remove</button>
+                </div>  
+            </div>`
     }
     document.getElementById("carts").innerHTML=str;
     document.getElementById("count").innerHTML=`${localStorage.length}`
@@ -24,4 +36,7 @@ function removeCart(id){
     // console.log(id);
     localStorage.removeItem(id);
     getCart();
+}
+function minus(){
+
 }
